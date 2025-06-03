3120d26201de1cb4c1c178458a2f0f65c6e64814
rileykraabel <u1354812@utah.edu>
2025-06-03 18:58:46 -0500

    end-state footer

--- react-portfolio/src/components/Footer.jsx
+++ react-portfolio/src/components/Footer.jsx
@@ -6,36 +6,45 @@ import './Footer.css';
 function Footer() {
     return (
         <div className="footer-main">
-            <div className="footer-header">
-                <h1> footer goes here! </h1>
-            </div>
             <div className="footer-content">
-                <p>Copyright &copy; 2025 Riley Kraabel</p>
                 <div className="footer-links">
-                    <div className="footer-section-links">
-                        <h2 className="footer-section-titles">Projects</h2>
-                        <a href="/">Project 1</a>
 
+                    <div className="footer-left">
+                        <p>&copy; rileykraabel.github.io </p>
                     </div>
-                    <div className="footer-section-links">
-                        <h2 className="footer-section-titles">Jump To</h2>
-                        <a href="/About">About</a>
-                        <a href="/projects">Projects</a>
-                        <a href="/contact">Contact</a>
+
+                    <div className="footer-center-left">
+                        <div className="footer-section">
+                            <h2 className="footer-section-titles">Projects</h2>
+                            <a href="/projects/#project-1">Project 1</a>
+                        </div>
                     </div>
-                    <div className="footer-section-links">
-                        <h2 className="footer-section-titles">Contact</h2>
-                        <a href="mailto:rileykraabel@gmail.com?subject=Let's%20Connect!">Email</a>
-                        <a href="https://www.linkedin.com/in/rileykraabel" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                        <a href="https://www.github.com/rileykraabel" target="_blank" rel="noopener noreferrer">GitHub</a>
-                        <a href={resume} target="_blank" rel="noopener noreferrer">Resume</a>
+
+                    <div className="footer-center-right">
+                        <div className="footer-section">
+                            <h2 className="footer-section-titles">Explore</h2>
+                            <a href="/about">About</a>
+                            <a href="/projects">Projects</a>
+                            <a href="/contact">Contact</a>
+                        </div>
                     </div>
-                </div>
+
+                    <div className="footer-right">
+                        <div className="footer-section">
+                            <h2 className="footer-section-titles">Contact</h2>
+                            <a href="mailto:rileykraabel@gmail.com?subject=Let's%20Connect!">Email</a>
+                            <a href="https://www.linkedin.com/in/rileykraabel" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                            <a href="https://www.github.com/rileykraabel" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            <a href={resume} target="_blank" rel="noopener noreferrer">Resume</a>
+                        </div>
+                    </div>
+                </div> 
             </div>
             <button onClick={scrollToTop} className="scroll-top-arrow" aria-label="Scroll to top">
                 ↑
             </button>
-        </div>
+            <p>this site was hand-crafted and designed by Riley Kraabel.</p>
+        </div >
     );
 }
 
